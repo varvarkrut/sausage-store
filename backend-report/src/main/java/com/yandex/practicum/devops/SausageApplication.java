c497a8df29a882f81062e2c7a191bcf680e0a6e2
Иван Шкурко <vanya.shcurko@yandex.com>
2023-01-14 22:57:05 +0000

    Add backend-report and docker-compose

--- backend-report/src/main/java/com/yandex/practicum/devops/SausageApplication.java
+++ backend-report/src/main/java/com/yandex/practicum/devops/SausageApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class SausageApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(SausageApplication.class, args);
     }
 
